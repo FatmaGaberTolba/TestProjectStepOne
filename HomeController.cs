f9870ed597087141a81037207ef375b10f37905a
FatmaGaberTolba <46868063+FatmaGaberTolba@users.noreply.github.com>
2019-12-16 13:09:23 +0300

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    class c
+    {
+        public int ID { set; get; }
+        public string Name { set; get; }
+    }
+
+    class product
+    {
+          public int ID { set; get; }
+          public string productName { set; get; }
+    }
+
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var obj =new List<c>{new c{ID=10, Name="ali"},new c{ID=20,Name="Mona"}};
+
+           
+         return Json(obj,JsonRequestBehavior.AllowGet);
+        }
+
+
+        public ActionResult GetProductByID(int ID) 
+        {
+            product p = new product { ID = ID, productName = "MyProduct" };
+            return Json(p, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
